6befde069652420d4a6f826465040d8289a93e88
Vusal Zarbaliyev <zarbaliyev.vusal@gmail.com>
2022-11-08 17:09:01 +0400

    Create entities with JPA

---
+++ src/main/java/com/avinty/hr/entity/DepartmentEntity.java
@@ -0,0 +1,45 @@
+package com.avinty.hr.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "DEPARTMENTS")
+@Data
+public class DepartmentEntity implements Serializable {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "manager_id")
+    private String managerId;
+
+    @OneToMany(targetEntity=EmployeeEntity.class, mappedBy="depId",cascade=CascadeType.ALL, fetch = FetchType.LAZY)
+    List<EmployeeEntity> employees;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @CreationTimestamp
+    @Column(name = "updated_at", nullable = false, updatable = false)
+    private LocalDateTime updateAt;
+
+    @Column(name = "updated_by", updatable = false)
+    private String updatedBy;
+}
---
+++ src/main/java/com/avinty/hr/entity/EmployeeEntity.java
@@ -0,0 +1,57 @@
+package com.avinty.hr.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "EMPLOYEES")
+@Data
+public class EmployeeEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    //@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "dep_id")
+    private int depId;
+
+    @ManyToOne
+    @JoinColumn(name = "dep_id", nullable = false, insertable = false, updatable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private DepartmentEntity department;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @CreationTimestamp
+    @Column(name = "updated_at", nullable = false, updatable = false)
+    private LocalDateTime updateAt;
+
+    @Column(name = "updated_by", updatable = false)
+    private String updatedBy;
+}
